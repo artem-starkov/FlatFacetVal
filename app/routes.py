cec744ee3e1ea1255517cc291b184f43b7111168
artem-starkov <91695372+artem-starkov@users.noreply.github.com>
2022-12-20 02:33:31 +0500

    fin commit
    added requirements.txt

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template, redirect, flash, jsonify, send_from_directory, request
+from flask import render_template, flash, send_from_directory, request
 from app.utils import nn_utils
+import os
 
 
 @app.route("/")
@@ -11,13 +12,25 @@ def home():
 @app.route('/validate_g5', methods=['GET', 'POST'])
 def validate_g5():
     if request.method == 'GET':
-        return render_template('validate_g10.html', title='Валидация g5', g=5)
+        return render_template('validate_g10.html', title='Валидация G5', g=5)
     else:
         case = int(request.form.get('case'))
         nn_type = int(request.form.get('model'))
         validation_data = nn_utils.validate(case, nn_type, 5)
         print(validation_data)
-        return render_template('validate_g10.html', title='Валидация g5', g=5, info=validation_data)
+        return render_template('validate_g10.html', title='Валидация G5', g=5, info=validation_data)
+
+
+@app.route('/validate_g10', methods=['GET', 'POST'])
+def validate_g10():
+    if request.method == 'GET':
+        return render_template('validate_g10.html', title='Валидация G10', g=10)
+    else:
+        case = int(request.form.get('case'))
+        nn_type = int(request.form.get('model'))
+        validation_data = nn_utils.validate(case, nn_type, 10)
+        print(validation_data)
+        return render_template('validate_g10.html', title='Валидация G10', g=10, info=validation_data)
 
 
 @app.route('/manual_input', methods=['GET', 'POST'])
@@ -25,12 +38,25 @@ def manual_input():
     if request.method == 'GET':
         return render_template('manual_input.html', title='Ручной ввод')
     else:
+        if not request.form.get('fi_rad') and not request.form.get('fi'):
+            flash('Угол не указан', 'fi')
+            return render_template('manual_input.html', title='Ручной ввод')
+        g = int(request.form.get('g'))
         nn_type = int(request.form.get('model'))
-        left_from = int(request.form.get('left_from'))    # 1
-        left_to = int(request.form.get('left_to'))        # 4
-        right_from = int(request.form.get('right_from'))  # 9 65
-        right_to = int(request.form.get('right_to'))      # 18 74
+        left_from = int(request.form.get('left_from'))    # 8
+        left_to = int(request.form.get('left_to'))        # 15
+        right_from = int(request.form.get('right_from'))  # 23 59
+        right_to = int(request.form.get('right_to'))      # 30 52
         r = float(request.form.get('r'))                  # 259.96702
-        fi = float(request.form.get('fi'))                # 1.539734
-        validation_data = nn_utils.validate_on_manual_input(nn_type, left_from, left_to, right_from, right_to, r, fi, 5)
+        if 'fi_rad' in request.form:
+            fi = float(request.form.get('fi_rad'))
+        else:
+            fi = float(request.form.get('fi'))
+        validation_data = nn_utils.validate_on_manual_input(nn_type, left_from, left_to, right_from, right_to, r, fi, g)
         return render_template('manual_input.html', title='Пользовательский ввод', info=validation_data)
+
+
+@app.route('/download_data')
+def download_data():
+    return send_from_directory(directory=os.path.join(app.root_path, 'static', 'reports'),
+                               path='data.csv', as_attachment=True)
--- app/utils/nn_utils.py
+++ app/utils/nn_utils.py
@@ -7,6 +7,8 @@ import math
 import numpy as np
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from sklearn.utils.validation import check_consistent_length, check_array
+import time
+import csv
 
 
 def get_model(model_type, prediction_type, g):
@@ -31,6 +33,7 @@ def get_model(model_type, prediction_type, g):
 
 def validate(test_case, nn_type, g):
     m = 1476
+    t = time.time()
     generator = Generator(g=g)
     dataset = generator.get_dataset_for_validating(m=m, test_case=test_case)
     if nn_type != NNType.NN1:
@@ -43,39 +46,65 @@ def validate(test_case, nn_type, g):
         x.append(precedent[:m])
         y_r.append(precedent[m])
         y_fi.append(precedent[m + 1])
+    gen_time = time.time() - t
+    t = time.time()
     dist_model = get_model(nn_type, PredictionType.Distance, g=g)
+    dist_time = time.time() - t
+    t = time.time()
     angle_model = get_model(nn_type, PredictionType.Azimuth, g=g)
+    angle_time = time.time() - t
+    t = time.time()
     y_r_pred = dist_model.predict(x)
+    dist_pred_time = time.time() - t
+    t = time.time()
     y_fi_pred = angle_model.predict(x)
     counter = 0
     for i in range(len(y_fi)):
         if abs(y_fi[i] - y_fi_pred[i]) < math.asin(g / y_r[i]):
             counter += 1
+    angle_pred_time = time.time() - t
+    save_to_file(y_r, y_r_pred, y_fi, y_fi_pred, g)
     return {
         'Distance': {'R2': r2_score(y_r, y_r_pred), '1-MAPE': (1 - mean_absolute_percentage_error(y_r, y_r_pred)) * 100,
                      'RMSE': mean_squared_error(y_r, y_r_pred, squared=False)},
         'Angle': {'R2': r2_score(y_fi, y_fi_pred),
                   '1-MAPE': (1 - mean_absolute_percentage_error(y_fi, y_fi_pred)) * 100,
-                  'RMSE': mean_squared_error(y_fi, y_fi_pred, squared=False), 'Hit rate': counter / len(y_fi)}}
+                  'RMSE': mean_squared_error(y_fi, y_fi_pred, squared=False), 'Hit rate': counter / len(y_fi)},
+        'angle_pred_time': angle_pred_time, 'angle_time': angle_time, 'dist_time': dist_time, 'gen_time': gen_time,
+        'dist_pred_time': dist_pred_time}
 
 
 def validate_on_manual_input(nn_type, left_from, left_to, right_from, right_to, r_true, fi_true, g):
-    m = 1476 if nn_type != NNType.NN3 else 164
+    if fi_true > 4:
+        fi_true /= 57.3
+    m = 164
     precedent = [0] * m
     for i in range(left_from - 1, left_to):
         precedent[i] = 1
     for i in range(int(m / 2) + right_from - 1, int(m / 2) + right_to):
         precedent[i] = 1
+    if nn_type != NNType.NN3:
+        precedent = [0] * 318 + precedent[:82] + [0] * 338 + [0] * 338 + precedent[82:] + [0] * 318
+    print(len(precedent))
+    t = time.time()
     dist_model = get_model(nn_type, PredictionType.Distance, g=g)
+    dist_time1 = time.time() - t
+    t = time.time()
     angle_model = get_model(nn_type, PredictionType.Azimuth, g=g)
+    angle_time1 = time.time() - t
+    t = time.time()
     r_pred = dist_model.predict([precedent])
+    dist_time2 = time.time() - t
+    t = time.time()
     fi_pred = angle_model.predict([precedent])
+    angle_time2 = time.time() - t
     return {
         'Distance': {'R2': r2_score([r_true], r_pred), '1-MAPE': (1 - mean_absolute_percentage_error([r_true], r_pred)) * 100,
                      'RMSE': mean_squared_error([r_true], r_pred, squared=False), 'Value': r_pred},
         'Angle': {'R2': r2_score([fi_true], fi_pred),
                   '1-MAPE': (1 - mean_absolute_percentage_error([fi_true], fi_pred)) * 100,
-                  'RMSE': mean_squared_error([fi_true], fi_pred, squared=False), 'Value': fi_pred}}
+                  'RMSE': mean_squared_error([fi_true], fi_pred, squared=False), 'Value': fi_pred},
+        'dist_time': dist_time1, 'angle_time': angle_time1, 'dist_pred_time': dist_time2, 'angle_pred_time': angle_time2}
 
 
 def mean_absolute_percentage_error(y_true, y_pred, sample_weight=None, multioutput='uniform_average'):
@@ -131,3 +160,13 @@ def check_reg_targets(y_true, y_pred, multioutput, dtype="numeric"):
     y_type = 'continuous' if n_outputs == 1 else 'continuous-multioutput'
 
     return y_type, y_true, y_pred, multioutput
+
+
+def save_to_file(r_true, r_pred, fi_true, fi_pred, g):
+    filename = os.path.join(app.root_path, 'static', 'reports', 'data.csv')
+    with open(filename, 'w', newline='') as output:
+        writer = csv.writer(output, delimiter=';')
+        writer.writerow(['R true', 'R pred', 'Fi true', 'Fi pred', 'Hit'])
+        for i in range(len(fi_true)):
+            hit = 1 if abs(fi_true[i] - fi_pred[i]) < math.asin(g / r_true[i]) else 0
+            writer.writerow([r_true[i], r_pred[i][0], fi_true[i], fi_pred[i][0], hit])
